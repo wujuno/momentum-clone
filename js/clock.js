32a0edf7f09bdc0fe72589db6d7c2ac21347ee0e
wujuno <86146780+wujuno@users.noreply.github.com>
2022-10-02 22:31:16 +0900

    Update clock.js
    clean code

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 const clock = document.querySelector(".main-clock");
-/* 왜 new Date 이렇게 하는지*/ 
+
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0");
